f6ac9d823057c5f2add4221079747c44fac1bdd9
liangsky <640634387@qq.com>
2024-04-05 14:23:17 +0800

    feat: config plugin use ts type

--- config/mock-server.ts
+++ config/mock-server.ts
@@ -1,8 +1,9 @@
 import mockServer from '@liangskyli/mock';
+import type { IPluginContext } from '@tarojs/service';
 
-export default (ctx: any) => {
+export default (ctx: IPluginContext) => {
   let isFirstWatch = true;
-  ctx.onBuildFinish(async (opts: any) => {
+  ctx.onBuildFinish(async (opts) => {
     let needStart = !opts.isWatch || isFirstWatch;
     if (needStart) {
       await mockServer({ port: 9001 });
