822d05552a7c4ec6dc02dca214faf878b2949233
liangsky <640634387@qq.com>
2024-04-29 20:43:44 +0800

    fix: eslint and stylelint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   extends: ['taro/react'],
+  parser: '@typescript-eslint/parser',
   rules: {
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,5 +2,11 @@ module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-recommended-less'],
   rules: {
     // your rules
+    'selector-type-no-unknown': [
+      true,
+      {
+        ignoreTypes: ['page'],
+      },
+    ],
   },
 };
